e3f2ae4cc2635b3fe0bef2758f59bd31032c9cf5
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-10-17 18:28:47 +0530

    c18

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,22 @@ const Navbar = () => {
             className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => setToggle(!toggle)}
           />
-          <div className={`${!toggle ? 'hidden' : 'flex'}`}>
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+          <ul className='list-none flex justify-end items-start flex-col gap-4'>
+          {navLinks.map((Link) => (
+            <li
+              key={Link.id}
+              className={`${
+                active === Link.title
+                  ? 'text-white'
+                  : 'text-secondary'
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(Link.title)}
+            >
+              <a href={`#${Link.id}`}>{Link.title}</a>
+            </li>
+          ))}
+        </ul>
 
           </div>
         </div>
