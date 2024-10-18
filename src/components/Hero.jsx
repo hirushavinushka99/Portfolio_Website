c1c99549f8f8f41837ec09cd3e61baa4f63abcfa
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-10-18 15:26:10 +0530

    c22

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,8 +12,12 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Hirusha</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I design user interfaces and build <br className='sm:block hidden' /> frontend web applications
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   )
 }
