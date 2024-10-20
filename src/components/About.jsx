89e29065b08bd616bcd755dc9c6821590b80d2e6
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-10-20 12:25:14 +0530

    c27

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
